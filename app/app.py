45547687b7726a7b85fbc59c555089f3a0b157a4
Francis Konikkara <96188342+franciskonikkara@users.noreply.github.com>
2026-01-02 19:36:30 -0500

    Refactor cmd route to use user_cmd variable

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
 # Intentional Vulnerable App
-
 from flask import Flask, request
 import os
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Secure CI/CD Demo"
+
 @app.route("/cmd")
 def cmd():
-    cmd = request.args.get("cmd")
-    return os.popen(cmd).read()
+    user_cmd = request.args.get("cmd")
+    return os.popen(user_cmd).read()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
